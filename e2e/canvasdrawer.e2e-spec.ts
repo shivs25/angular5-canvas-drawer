a38a61b2b78b60ab2c0f0b93454ad9b1154dbe2e
Jacob Leslie <Jacob@sitesusa.local>
2018-03-05 11:17:32 -0700

    Added test spec template

---
+++ e2e/canvasdrawer.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { } from './jasmine';
+import { promise, browser, element, by, protractor } from 'protractor';
+const protractorImageComparison = require('protractor-image-comparison');
+
+describe('Maps', function () {
+  
+    beforeAll(() => {
+        //browser.ignoreSynchronization = true;
+
+    });
+    afterAll(() => {
+
+    });
+    beforeEach(() => {
+        browser.get('/');
+
+    });
+    afterEach(() => {
+        
+    });
+
+
+    it('CanvasDrawer_SCENARIO_EXPECTATION', () => {
+
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    });
+});
