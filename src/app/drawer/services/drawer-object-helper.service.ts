82df36c088e9268917306ce5ce5aa762f3f7c702
Robert Leon Collins Jr <robertleoncollinsjr@Roberts-MacBook-Pro-2.local>
2018-03-28 15:11:02 -0700

    Added two new helper functions for rotated objects

--- src/app/drawer/services/drawer-object-helper.service.ts
+++ src/app/drawer/services/drawer-object-helper.service.ts
@@ -148,55 +148,125 @@ export class DrawerObjectHelperService {
 
     return boundingBox;
   }
+
+
+  public getRotatedPoint(x: number, y: number, originX: number, originY: number, angle: number): DrPoint {
+    let radians = angle * Math.PI / 180;
+    let newX = Math.cos(radians) * (x - originX) - Math.sin(radians) * (y - originY) + originX;
+    let newY = Math.sin(radians) * (x - originX) + Math.cos(radians) * (y - originY) + originY;
+    return {
+      x: newX,
+      y: newY
+    };
+  }
+
+  public getRotatedBoundingBox(bb: BoundingBox, rotation: number): BoundingBox {
+    let returnValue: BoundingBox = createBoundingBox();
+    //top left
+    let x1: number = bb.x;
+    let y1: number = bb.y;
+    //bottom left
+    let x2: number = bb.x;
+    let y2: number = bb.y + bb.width;
+    //top right
+    let x3: number = bb.x + bb.width;
+    let y3: number = bb.y;
+    //bottom right
+    let x4: number = bb.x + bb.width;
+    let y4: number = bb.y + bb.height;
+    //Center of boundingbox
+    let centerX: number = bb.x + (bb.width / 2);
+    let centerY: number = bb.y + (bb.height / 2);
+    
+    let point1: DrPoint = this.getRotatedPoint(x1,y1,centerX,centerY,rotation);
+    let point2: DrPoint = this.getRotatedPoint(x2,y2,centerX,centerY,rotation);
+    let point3: DrPoint = this.getRotatedPoint(x3,y3,centerX,centerY,rotation);
+    let point4: DrPoint = this.getRotatedPoint(x4,y4,centerX,centerY,rotation);
+
+    let points: DrPoint[] = [point1, point2, point3, point4];
+
+    let minX: number = 0;
+    let minY: number = 0;
+    let maxX: number = 0;
+    let maxY: number = 0;
+    for(let k = 0; k < points.length; k++){
+      if(k === 0){
+        minX = points[k].x;
+        minY = points[k].y;
+        maxY = points[k].y;
+        maxX = points[k].x;
+      } else {
+        if(minX > points[k].x){
+          minX = points[k].x;
+        }
+        if(maxX < points[k].x){
+          maxX = points[k].x;
+        }
+        if(minY > points[k].y){
+          minY = points[k].y;
+        }
+        if(maxY < points[k].y){
+          maxY = points[k].y;
+        }
+      }
+    }
+
+    returnValue.y = minY;
+    returnValue.x = minX;
+    returnValue.height = maxY - minY;
+    returnValue.width = maxX - minX;
+    return returnValue;
+  }
+
                                 //currently set to any because when trying to access individual items it errored on compilation
   private getBoundingBoxForObject(drObj: any): BoundingBox {
     let boundingBox: BoundingBox =  createBoundingBox();
     switch(drObj.drType){
       case(DrType.ELLIPSE):
-        boundingBox.y = drObj.y - drObj.ry;
-        boundingBox.x = drObj.x - drObj.rx;
-        boundingBox.height = drObj.ry * 2;
-        boundingBox.width = drObj.rx * 2;
+          boundingBox.y = drObj.y - drObj.ry;
+          boundingBox.x = drObj.x - drObj.rx;
+          boundingBox.height = drObj.ry * 2;
+          boundingBox.width = drObj.rx * 2;
       break;
       case(DrType.POLYGON):
-      let minX: number = 0;
-      let minY: number = 0;
-      let maxX: number = 0;
-      let maxY: number = 0;
-      for(let k = 0; k < drObj.points.length; k++){
-        if(k === 0){
-          minX = drObj.points[k].x;
-          minY = drObj.points[k].y;
-          maxY = drObj.points[k].y;
-          maxX = drObj.points[k].x;
-        } else {
-          if(minX > drObj.points[k].x){
-            minX = drObj.points[k].x;
+          let minX: number = 0;
+          let minY: number = 0;
+          let maxX: number = 0;
+          let maxY: number = 0;
+          for(let k = 0; k < drObj.points.length; k++){
+            if(k === 0){
+              minX = drObj.points[k].x;
+              minY = drObj.points[k].y;
+              maxY = drObj.points[k].y;
+              maxX = drObj.points[k].x;
+            } else {
+              if(minX > drObj.points[k].x){
+                minX = drObj.points[k].x;
+              }
+              if(maxX < drObj.points[k].x){
+                maxX = drObj.points[k].x;
+              }
+              if(minY > drObj.points[k].y){
+                minY = drObj.points[k].y;
+              }
+              if(maxY < drObj.points[k].y){
+                maxY = drObj.points[k].y;
+              }
+            }
           }
-          if(maxX < drObj.points[k].x){
-            maxX = drObj.points[k].x;
-          }
-          if(minY > drObj.points[k].y){
-            minY = drObj.points[k].y;
-          }
-          if(maxY < drObj.points[k].y){
-            maxY = drObj.points[k].y;
-          }
-        }
-      }
 
-      boundingBox.y = minY;
-      boundingBox.x = minX;
-      boundingBox.height = maxY - minY;
-      boundingBox.width = maxX - minX;
+          boundingBox.y = minY;
+          boundingBox.x = minX;
+          boundingBox.height = maxY - minY;
+          boundingBox.width = maxX - minX;
       break;
       case(DrType.IMAGE):
       case(DrType.TEXT):
       case(DrType.RECTANGLE):
-        boundingBox.y = drObj.y;
-        boundingBox.x = drObj.x;
-        boundingBox.width = drObj.width;
-        boundingBox.height = drObj.height;
+          boundingBox.y = drObj.y;
+          boundingBox.x = drObj.x;
+          boundingBox.width = drObj.width;
+          boundingBox.height = drObj.height;
       break;
       case (DrType.GROUPED_OBJECT): {
         boundingBox = this.getBoundingBox(drObj.objects);
@@ -207,10 +277,5 @@ export class DrawerObjectHelperService {
 
     return boundingBox;
   }
-
-  
-  
-
-  
 
 }
