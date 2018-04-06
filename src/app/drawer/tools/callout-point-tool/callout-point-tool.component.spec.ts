1021901e225828338249d829c21187c6490212c1
Robert Collins Jr <robbie@sitesusa.local>
2018-04-06 13:37:09 -0700

    Updated unit tests

--- src/app/drawer/tools/callout-point-tool/callout-point-tool.component.spec.ts
+++ src/app/drawer/tools/callout-point-tool/callout-point-tool.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataStoreService } from '../../services/data-store.service';
 
 import { CalloutPointToolComponent } from './callout-point-tool.component';
 
 describe('CalloutPointToolComponent', () => {
   let component: CalloutPointToolComponent;
   let fixture: ComponentFixture<CalloutPointToolComponent>;
-
+  let dataService: any = {};
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalloutPointToolComponent ]
+      declarations: [CalloutPointToolComponent],
+      providers: [{ provide: DataStoreService, useValue: dataService },
+]
     })
     .compileComponents();
   }));
