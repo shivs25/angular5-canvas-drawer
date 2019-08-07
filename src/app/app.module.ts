cc0b3330e6c1350497c48eb1cad6976436046190
Robbie Collins <robbie@Robbies-iMac.local>
2019-08-07 09:48:11 -0700

    Trying to fix a broken export

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { DrawerLibraryModule } from './drawer/drawer-library.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataStoreService } from './drawer/services/data-store.service';
 import { DrawerObjectHelperService } from './drawer/services/drawer-object-helper.service';
 import { ElementRef } from '@angular/core';
 
@@ -14,7 +15,7 @@ import { ElementRef } from '@angular/core';
     BrowserModule,
     DrawerLibraryModule.forRoot()
   ],
-  providers: [DrawerObjectHelperService],
+  providers: [DrawerObjectHelperService, DataStoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
